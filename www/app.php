75bc9dd2a4667fa085978b7b3542110af92dbe02
Анна <annaalexandrovnas@yandex.ru>
2015-11-06 22:03:38 +0300

    3 ДЗ

---
+++ www/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => '<em>ООО "Магазин"</em><br>',
+    'contacts' => [
+        'tel' => '<a href="88001000000">8(800)100-00-00</a><br>',
+        'address' => '<address>Красная пл, д.1</address>'
+
+    ],
+
+];
